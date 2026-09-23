48775e44cd13174cbe16ec3582223994858f3f15
Sujal Pahwa <sujalpahwa4@gmail.com>
2026-09-23 23:47:01 +0530

    feat: implemented part of pushing code on aws

--- build-server/script.js
+++ build-server/script.js
@@ -1,3 +1,64 @@
-async function init() {
-    
+const { exec } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const mime = require('mime-types');
+
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY 
+    }
+});
+
+const PROJECT_ID = process.env.PROJECT_ID || 'default_project_id';
+
+async function init() { 
+    console.log('Executing script.js');
+    publishLog('Build Started...');
+    const outDirPath = path.join(__dirname, 'output');
+
+    const p = exec(`cd ${outDirPath} && npm install && npm run build`)
+
+    p.stdout.on('data', function (data) {
+        console.log(data.toString());
+        publishLog(data.toString());
+    });
+
+    p.stdout.on('data', function (data) {
+        console.log('Error', data.toString());
+        publishLog('Error: ' + data.toString());
+    });
+
+    p.on('close', async function () {
+        console.log('Build Complete');
+        publishLog(`Build Complete`);
+        const distFolderPath = path.join(__dirname, 'output', 'dist');
+        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true });
+
+        publishLog(`Starting to upload`)
+        for (const file of distFolderContents) {
+            const filePath = path.join(distFolderPath, file);
+            if (fs.lstatSync(filePath).isDirectory()) continue; 
+
+            console.log(`uploading`, filePath);
+            publishLog(`uploading ${file}`);
+
+            const command = new PutObjectCommand({
+                Bucket: 'pegasus-outputs',
+                Key: `__outputs/${PROJECT_ID}/${file}`,
+                Body: fs.createReadStream(filePath),
+                ContentType: mime.lookup(filePath) 
+            })
+
+            await s3Client.send(command);
+            publishLog(`uploaded ${file}`);
+            console.log(`uploaded`, filePath); 
+        }
+        publishLog(`Done`);
+        console.log('Done...'); 
+    });
 }
+
+init(); 
